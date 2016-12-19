4dafd2253b4bbb4e5158aac7918c5e79f1eafc44
Gary Pang <gary.pang@gmail.com>
2016-12-19 15:50:31 -0500

    Refactor test
    Nest test suite for non-positive numbers. Add test suite to check for
    numbers divisible by 3.

--- test/validator-spec.js
+++ test/validator-spec.js
@@ -5,14 +5,26 @@ var chai = require('chai'),
 
 describe('A Validator', function() {
 	it('will return no errors for valid numbers', function() {
-		expect(validator(3)).to.be.empty;
+		expect(validator(7)).to.be.empty;
 	});
 
-	it('will return error.nonpositive for not strictly positive numbers, like 0', function() {
-		expect(validator(0)).to.be.deep.equal(['error.nonpositive']);
+	describe('will return error.nonpositive for not strictly positive numbers: ', function() {
+		it('like 0', function() {
+			expect(validator(0)).to.be.deep.equal(['error.nonpositive']);
+		});
+
+		it('like -2', function() {
+			expect(validator(-2)).to.be.deep.equal(['error.nonpositive']);
+		});
 	});
 
-	it('will return error.nonpositive for not strictly positive numbers, like -2', function() {
-		expect(validator(-2)).to.be.deep.equal(['error.nonpositive']);
+	describe('will return error.three for divisible by 3 numbers: ', function() {
+		it('like 3', function() {
+			expect(validator(3)).to.be.deep.equal(['error.three']);
+		});
+
+		it('like 6', function() {
+			expect(validator(6)).to.be.deep.equal(['error.three']);
+		});
 	});
 });
