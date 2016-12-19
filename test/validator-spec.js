76933f8316c3c731b794e8b80286a3cbccce0af3
Gary Pang <gary.pang@gmail.com>
2016-12-19 15:03:18 -0500

    Fix Hello World test
    Test works correctly.

--- test/validator-spec.js
+++ test/validator-spec.js
@@ -1,9 +1,18 @@
-var assert = require('assert'),
+var chai = require('chai'),
+	expect = chai.expect,
 	validator = require('../lib/validator');
-	
+
 
 describe('A Validator', function() {
-	it('will return error.nonpositive for not strictly positive numbers', function() {
-		assert.deepEqual(validator(0), ['error.nonpositive']);
+	it('will return no errors for valid numbers', function() {
+		expect(validator(3)).to.be.empty;
+	});
+
+	it('will return error.nonpositive for not strictly positive numbers, like 0', function() {
+		expect(validator(0)).to.be.deep.equal(['error.nonpositive']);
+	});
+
+	it('will return error.nonpositive for not strictly positive numbers, like -2', function() {
+		expect(validator(-2)).to.be.deep.equal(['error.nonpositive']);
 	});
 });
