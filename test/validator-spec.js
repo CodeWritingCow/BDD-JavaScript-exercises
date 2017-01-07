8e392302d46cf99fc33e8903f29cf6a19d7c7e42
Gary Pang <gary.pang@gmail.com>
2017-01-07 15:55:32 -0500

    Refactor code
    Replace duplicate code with validator( ).

--- test/validator-spec.js
+++ test/validator-spec.js
@@ -6,71 +6,48 @@ var chai = require('chai'),
 
 
 describe('A Validator', function() {
-	it('will return no errors for valid numbers', function() {
-		var validator = validatorWith([
+	var validator;
+	// beforeEach runs once before each it( ). Use this by default.
+	// before runs only once. use for testing an immutable object or doing a one-time setup.
+	// beforeEach(function() {
+	before(function() {
+		validator = validatorWith([
 			nonPositiveValidationRule,
 			nonDivisibleValidationRule(3, 'error.three'),
-			nonDivisibleValidationRule(5, 'error.five'),
+			nonDivisibleValidationRule(5, 'error.five')
 			]);
+	});
+
+	it('will return no errors for valid numbers', function() {
 		expect(validator(7)).to.be.empty;
 	});
 
 	describe('will include error.nonpositive for not strictly positive numbers: ', function() {
 		it('like 0', function() {
-			var validator = validatorWith([
-				nonPositiveValidationRule,
-				nonDivisibleValidationRule(3, 'error.three'),
-				nonDivisibleValidationRule(5, 'error.five')
-				]);
-		expect(validator(0)).to.include('error.nonpositive');
+			expect(validator(0)).to.include('error.nonpositive');
 		});
 
 		it('like -2', function() {
-			var validator = validatorWith([
-				nonPositiveValidationRule,
-				nonDivisibleValidationRule(3, 'error.three'),
-				nonDivisibleValidationRule(5, 'error.five')
-				]);
-		expect(validator(-2)).to.include('error.nonpositive');
+			expect(validator(-2)).to.include('error.nonpositive');
 		});
 	});
 
 	describe('will include error.three for divisible by 3 numbers: ', function() {
 		it('like 3', function() {
-			var validator = validatorWith([
-				nonPositiveValidationRule,
-				nonDivisibleValidationRule(3, 'error.three'),
-				nonDivisibleValidationRule(5, 'error.five')
-				]);
 			expect(validator(3)).to.include('error.three');
 		});
 
 		it('like 15', function() {
-			var validator = validatorWith([
-				nonPositiveValidationRule,
-				nonDivisibleValidationRule(3, 'error.three'),
-				nonDivisibleValidationRule(5, 'error.five')
-			]);
 			expect(validator(15)).to.include('error.three');
 		});
 	});
 
 	describe('will include error.five for divisible by 5 numbers: ', function() {
 		it('like 5', function() {
-			var validator = validatorWith([
-				nonPositiveValidationRule,
-				nonDivisibleValidationRule(3, 'error.three'),
-				nonDivisibleValidationRule(5, 'error.five')
-				]);
 			expect(validator(5)).to.include('error.five');
 		});
 
 		it('like 15', function() {
-			var validator = validatorWith([
-				nonPositiveValidationRule,
-				nonDivisibleValidationRule(3, 'error.three'),
-				nonDivisibleValidationRule(5, 'error.five')
-				]);
 			expect(validator(15)).to.include('error.five');
 		});
 	});
