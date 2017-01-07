5ea50ddbf98a8368f58e71af3573a04f398daa4c
Gary Pang <gary.pang@gmail.com>
2017-01-07 16:36:55 -0500

    Add context
    Reorganize code. Separate test suites into two contexts: default rules;
    and with other rules.

--- test/validator-spec.js
+++ test/validator-spec.js
@@ -7,48 +7,54 @@ var chai = require('chai'),
 
 describe('A Validator', function() {
 	var validator;
-	// beforeEach runs once before each it( ). Use this by default.
-	// before runs only once. use for testing an immutable object or doing a one-time setup.
-	// beforeEach(function() {
-	before(function() {
-		validator = validatorWith([
-			nonPositiveValidationRule,
-			nonDivisibleValidationRule(3, 'error.three'),
-			nonDivisibleValidationRule(5, 'error.five')
-			]);
+	context('with the default validation rules', function() {
+		beforeEach(function() {
+			validator = validatorWith([
+				nonPositiveValidationRule,
+				nonDivisibleValidationRule(3, 'error.three'),
+				nonDivisibleValidationRule(5, 'error.five')
+				]);
+		});
+
+		it('will return no errors for valid numbers', function() {
+			expect(validator(7)).to.be.empty;
+		});
+
+		describe('will include error.nonpositive for not strictly positive numbers: ', function() {
+			it('like 0', function() {
+				expect(validator(0)).to.include('error.nonpositive');
+			});
+
+			it('like -2', function() {
+				expect(validator(-2)).to.include('error.nonpositive');
+			});
+		});
+
+		describe('will include error.three for divisible by 3 numbers: ', function() {
+			it('like 3', function() {
+				expect(validator(3)).to.include('error.three');
+			});
+
+			it('like 15', function() {
+				expect(validator(15)).to.include('error.three');
+			});
+		});
+
+		describe('will include error.five for divisible by 5 numbers: ', function() {
+			it('like 5', function() {
+				expect(validator(5)).to.include('error.five');
+			});
+
+			it('like 15', function() {
+				expect(validator(15)).to.include('error.five');
+			});
+		});
 	});
 
-	it('will return no errors for valid numbers', function() {
-		expect(validator(7)).to.be.empty;
-	});
-
-	describe('will include error.nonpositive for not strictly positive numbers: ', function() {
-		it('like 0', function() {
-			expect(validator(0)).to.include('error.nonpositive');
+	context('with other rules', function() {
+		beforeEach(function() {
+			validator = validatorWith([weirdRule(1), nonStandardRule]);
 		});
-
-		it('like -2', function() {
-			expect(validator(-2)).to.include('error.nonpositive');
-		});
-	});
-
-	describe('will include error.three for divisible by 3 numbers: ', function() {
-		it('like 3', function() {
-			expect(validator(3)).to.include('error.three');
-		});
-
-		it('like 15', function() {
-			expect(validator(15)).to.include('error.three');
-		});
-	});
-
-	describe('will include error.five for divisible by 5 numbers: ', function() {
-		it('like 5', function() {
-			expect(validator(5)).to.include('error.five');
-		});
-
-		it('like 15', function() {
-			expect(validator(15)).to.include('error.five');
-		});
+		// Other tests
 	});
 });
